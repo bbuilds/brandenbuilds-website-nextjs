9f03255c8bea6d0b5f3da4d5436b1a200dbed5eb
bbuilds <hello@brandenbuilds.com>
2023-11-21 12:38:48 -0500

    fix: broken MDX renderer

--- components/MDXComponent.js
+++ components/MDXComponent.js
@@ -1,21 +1,21 @@
 /* eslint-disable react/display-name */
-import { useMemo } from 'react';
-import { getMDXComponent } from 'mdx-bundler/client';
-import Image from 'next/image';
-import Link from 'next/link';
-import TOCInline from './TOCInline';
+import { useMemo } from "react";
+import { getMDXComponent } from "mdx-bundler/client";
+import Image from "next/image";
+import Link from "next/link";
+import TOCInline from "@/components/TOCInline";
+import Layout from "@/layouts/PostLayout";
 
 export const MDXComponents = {
-	Image,
-	Link,
-	TOCInline,
-	wrapper: ({ components, layout, ...rest }) => {
-		const Layout = require(`../layouts/${layout}`).default;
-		return <Layout {...rest} />;
-	}
+  Image,
+  Link,
+  TOCInline,
+  wrapper: ({ components, layout, ...rest }) => {
+    return <Layout {...rest} />;
+  },
 };
 
 export const MDXLayoutRenderer = ({ layout, mdxSource, ...rest }) => {
-	const MDXLayout = useMemo(() => getMDXComponent(mdxSource), [mdxSource]);
-	return <MDXLayout layout={layout} components={MDXComponents} {...rest} />;
+  const MDXLayout = useMemo(() => getMDXComponent(mdxSource), [mdxSource]);
+  return <MDXLayout layout={layout} components={MDXComponents} {...rest} />;
 };
