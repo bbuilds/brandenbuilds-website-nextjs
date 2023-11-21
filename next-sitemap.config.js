76df6fa85e09cf6894bca61f789a4905e1c4fc0f
bbuilds <hello@brandenbuilds.com>
2023-11-21 11:57:51 -0500

    refactor: :recycle: move sitemap to nextsitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://brandenbuilds.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: []
-	}
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 });
 
 module.exports = withMDX({
-	reactStrictMode: true,
-	pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-	webpack(config, { isServer }) {
-		if (isServer) {
-			require('./scripts/generate-sitemap');
-		}
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack']
-		});
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-		return config;
-	}
+    return config;
+  },
 });
